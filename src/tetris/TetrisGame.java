36c93fc9f192fd28b89e081390874fefb3e3b44f
JoBanana123 <joanna.lin@smus.ca>
2024-05-14 11:22:59 -0700

    make small changes

--- src/tetris/TetrisGame.java
+++ src/tetris/TetrisGame.java
@@ -138,7 +138,7 @@ public class TetrisGame extends JPanel implements ActionListener{
 	
 	public void actionPerformed(ActionEvent e) {
 		time ++;
-		//bloci moves down slowly
+		//block moves down slowly
 		if (time % 50 == 0) {
 			y += v;
 		}
@@ -180,6 +180,20 @@ public class TetrisGame extends JPanel implements ActionListener{
 	    			}
 	    			break;
 	    			
+	    			// drop	
+	    		case KeyEvent.VK_DOWN:
+	    			int j = (x-20) / 30;
+	    			
+	    			for (int i = 19; i >= 0; i--) {
+	    				if (board[i][j] == 0) {
+	    					board[i][j] = 1;
+	    					i = -1;
+	    				}
+	    			}
+	    			x = 140;
+	    			y = 60;
+	    			break;
+	    			
 	    		case KeyEvent.VK_A:
 	    			if (x == 20) {
 	    				x -=0;
@@ -200,11 +214,11 @@ public class TetrisGame extends JPanel implements ActionListener{
 	    			
 	    		// drop	
 	    		case KeyEvent.VK_S:
-	    			int j = (x-20) / 30;
+	    			int k = (x-20) / 30;
 	    			
 	    			for (int i = 19; i >= 0; i--) {
-	    				if (board[i][j] == 0) {
-	    					board[i][j] = 1;
+	    				if (board[i][k] == 0) {
+	    					board[i][k] = 1;
 	    					i = -1;
 	    				}
 	    			}
@@ -231,7 +245,6 @@ public class TetrisGame extends JPanel implements ActionListener{
 
 	//main function
 	public static void main(String[] args) {
-		
 		//set title
 		JFrame window = new JFrame("Tetris");
 		
