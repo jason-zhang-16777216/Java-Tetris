f2bf87c333f3464588e4e4760d998cf7295a2d84
JoBanana123 <joanna.lin@smus.ca>
2024-06-06 14:35:53 -0700

    added audio

---
+++ src/tetris/AudioPlayer.java
@@ -0,0 +1,29 @@
+package tetris;
+
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.*;
+
+public class AudioPlayer {
+	private static Clip song; 
+
+	private static void addSong() {
+		try {
+			File bgmusic = new File("src/tetris/audio/game-music-loop-3-144252.wav");
+			if (bgmusic == null) {
+				throw new IOException("Sound File not Found");
+			}
+			AudioInputStream input = AudioSystem.getAudioInputStream(bgmusic);
+            song = AudioSystem.getClip();
+            song.open(input);
+            song.start();
+            // If you want to loop the audio
+            song.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+	}
+    public static void main(String[] args) {
+    	addSong();
+    }
+}
